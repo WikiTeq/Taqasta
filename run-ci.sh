1089639b6a36bd19ed90bdaf08bbf04e0387b2b7
Vedmaka <god.vedmaka@gmail.com>
2023-08-14 02:33:56 +0400

    Test

--- run-ci.sh
+++ run-ci.sh
@@ -25,6 +25,8 @@ php maintenance/update.php --quick > /dev/null
 #php tests/phpunit/phpunit.php --stop-on-failure --stop-on-error --testsuite maintenance_suite
 #php tests/phpunit/phpunit.php --stop-on-failure --stop-on-error --testsuite parsertests
 #php tests/phpunit/phpunit.php --stop-on-failure --stop-on-error --testsuite languages
+cat tests/phpunit/includes/HooksTest.php | grep -A 5 testCallHook_Deprecated
+cat tests/phpunit/includes/HooksTest.php | grep -A 5 "function someStatic"
 php -v
 #php -i
 php tests/phpunit/phpunit.php --stop-on-failure --stop-on-error --exclude-group Dump,Broken
