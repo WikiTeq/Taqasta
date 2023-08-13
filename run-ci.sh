2a949a104bb5ef9f810d8017e4df74c142d81e3a
Vedmaka <god.vedmaka@gmail.com>
2023-08-13 23:32:18 +0400

    Test

--- run-ci.sh
+++ run-ci.sh
@@ -1,19 +1,29 @@
 #!/bin/bash
 
-apt update
-apt install -y php7.4-sqlite3 sqlite3 sqlitebrowser
-composer update
-php maintenance/install.php --dbtype sqlite --dbuser root --dbname mw --dbpath $(pwd) --pass AdminPassword WikiName AdminUser
+# Setup
+apt -qq update
+apt -qq install -y php7.4-sqlite3 sqlite3 sqlitebrowser nodejs npm
+composer -n --quiet update
+
+composer run-script mw-install:sqlite
+#php maintenance/install.php --dbtype sqlite --dbuser root --dbname mw --dbpath $(pwd) --pass AdminPassword WikiName AdminUser
+
 echo 'error_reporting(0);' >> LocalSettings.php
 echo 'wfLoadExtension("Bootstrap");' >> LocalSettings.php
 echo '$wgShowExceptionDetails = false;' >> LocalSettings.php
 echo '$wgShowDBErrorBacktrace = false;' >> LocalSettings.php
 echo '$wgDevelopmentWarnings = false;' >> LocalSettings.php
+
 php maintenance/update.php --quick
+
+# PHPUnit
 #php tests/phpunit/phpunit.php --stop-on-failure --stop-on-error --testsuite integration
 #php tests/phpunit/phpunit.php --stop-on-failure --stop-on-error --testsuite documentation
 #php tests/phpunit/phpunit.php --stop-on-failure --stop-on-error --testsuite tests
 #php tests/phpunit/phpunit.php --stop-on-failure --stop-on-error --testsuite maintenance_suite
 #php tests/phpunit/phpunit.php --stop-on-failure --stop-on-error --testsuite parsertests
 #php tests/phpunit/phpunit.php --stop-on-failure --stop-on-error --testsuite languages
-php tests/phpunit/phpunit.php --stop-on-failure --stop-on-error
+composer run-script test
+composer run-script phpunit
+
+# Qunit
