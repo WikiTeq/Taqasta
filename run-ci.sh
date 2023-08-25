d8e4ba4b9511eab82625923fd08b07b642d7e76e
Vedmaka <god.vedmaka@gmail.com>
2023-08-25 21:08:16 +0400

    Test

--- run-ci.sh
+++ run-ci.sh
@@ -61,6 +61,8 @@ echo "Running tests..."
 #cat tests/phpunit/includes/HooksTest.php | grep -A 5 "function someStatic"
 #php tests/phpunit/phpunit.php -c tests/phpunit/suite.xml --stop-on-failure --stop-on-error
 
-composer phpunit:entrypoint -- --stop-on-failure --stop-on-error
+# replicate WMF Jenkins command
+composer run --timeout=0 phpunit:entrypoint -- --testsuite extensions --group Database --exclude-group Broken,ParserFuzz,Stub,Standalone --stop-on-failure --stop-on-error
+#composer phpunit:entrypoint -- --stop-on-failure --stop-on-error
 
 # Qunit
