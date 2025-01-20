1f97798e6c1081d7980f0ecc78a2dfafece8f089
Daniel Scherzer <daniel@wikiteq.com>
2025-01-19 21:38:40 -0800

    [e2e] Add tests for authorization and rights
    Also run retries when running locally in docker

---
+++ e2e/tests/005-admin.spec.ts
@@ -0,0 +1,95 @@
+import {test, expect, defineConfig} from '@playwright/test';
+
+export default defineConfig({
+    timeout: 5 * 60 * 1000,  // 5 minutes per test
+});
+
+// Admin user that gets created by Taqasta
+const ADMIN_USER_NAME = 'Admin';
+const ADMIN_USER_PASS = 'rand0mmysqlpassw0rd1!';
+
+test('can log in to existing account', async ({page}) => {
+    await page.goto('/wiki/Special:UserLogin');
+
+    // Username
+    await expect(page.locator('#wpName1')).toHaveCount(1);
+    await page.locator('#wpName1').fill( ADMIN_USER_NAME );
+
+    // Password
+    await expect(page.locator('#wpPassword1')).toHaveCount(1);
+    await page.locator('#wpPassword1').fill( ADMIN_USER_PASS );
+
+    // Submit the form
+    await expect(page.locator('#wpLoginAttempt')).toHaveCount(1);
+    await expect(page.locator('#wpLoginAttempt')).toBeEnabled();
+    await page.locator('#wpLoginAttempt').click();
+
+    // Check the login - use Special:MyPage redirect
+    await page.goto('/wiki/Special:MyPage');
+    await expect(page.locator('#firstHeading')).toContainText("User:" + ADMIN_USER_NAME);
+});
+
+test('can grant user rights', async ({page}) => {
+    // Need to log in
+    await page.goto('/wiki/Special:UserLogin');
+    await page.locator('#wpName1').fill( ADMIN_USER_NAME );
+    await page.locator('#wpPassword1').fill( ADMIN_USER_PASS );
+    await page.locator('#wpLoginAttempt').click();
+
+    // And now, the actual test
+    await page.goto('/wiki/Special:UserRights/PlaywrightTester');
+
+    // Admin
+    await expect(page.locator('#wpGroup-sysop')).toHaveCount(1);
+    await page.locator('#wpGroup-sysop').check();
+
+    // Reason
+    await expect(page.locator('#wpReason')).toHaveCount(1);
+    await page.locator('#wpReason').fill( 'Grant adminship' );
+
+    // Submit the form
+    await expect(page.locator('input[name="saveusergroups"]')).toHaveCount(1);
+    await expect(page.locator('input[name="saveusergroups"]')).toBeEnabled();
+    await page.locator('input[name="saveusergroups"]').click();
+});
+
+test('can delete pages', async ({page}) => {
+    // Need to log in
+    await page.goto('/wiki/Special:UserLogin');
+    await page.locator('#wpName1').fill( 'PlaywrightTester' );
+    await page.locator('#wpPassword1').fill( 'dockerpass' );
+    await page.locator('#wpLoginAttempt').click();
+
+    // Create the page first
+    await page.goto('/wiki/Deletion_test?action=edit');
+    await expect(page.locator('#wpTextbox1')).toHaveCount(1);
+    await page.locator('#wpTextbox1').fill('Some content');
+    await expect(page.locator('#wpSave')).toHaveCount(1);
+    await expect(page.locator('#wpSave')).toBeEnabled();
+    await page.locator('#wpSave').click();
+
+    // Go to delete page, should exist and have delete button
+    await page.goto('/wiki/Deletion_test');
+    await expect(page.locator('#mw-content-text')).toContainText('Some content');
+    // Need to make the menu show up, otherwise element is not visible
+    await expect(page.locator('#p-cactions-checkbox')).toHaveCount(1);
+    await page.locator('#p-cactions-checkbox').check();
+    await expect(page.locator('#ca-delete > a')).toHaveCount(1);
+    await page.locator('#ca-delete > a').click();
+
+    // Deletion form, use default reason
+    await expect(page).toHaveTitle(/Delete "Deletion test"/);
+    await expect(page.locator('button[name="wpConfirmB"]')).toHaveCount(1);
+    await expect(page.locator('button[name="wpConfirmB"]')).toBeEnabled();
+    await page.locator('button[name="wpConfirmB"]').click();
+
+    // Go back to page, should be deleted
+    await page.goto('/wiki/Deletion_test');
+    // Not checking based on content text since the deletion logs have the text
+    await expect(page.locator('.noarticletext')).toHaveCount(1);
+    // Should have at least one log entry, allow multiple for running tests locally
+    const logLocator = page.locator('ul.mw-logevent-loglines li.mw-logline-delete');
+    await expect(logLocator.first()).toBeVisible();
+    const logCount = await logLocator.count();
+    expect(logCount).toBeGreaterThanOrEqual(1);
+});
