0c9efe99e7902251e6865780aaf01107872cf4d7
Daniel Scherzer <daniel@wikiteq.com>
2025-01-19 17:19:45 -0800

    [e2e] Reduce use of environmental variables for configuration

--- e2e/LocalSettings.php
+++ e2e/LocalSettings.php
@@ -7,5 +7,22 @@ if ( !defined( 'MEDIAWIKI' ) ) {
 	exit;
 }
 
+// General settings that could be done via the environment but aren't because
+// normally wikis put their settings in PHP
+$wgCacheDirectory = false;
+$wgSecretKey = 'jf843y8973098waujda9odu89seyf9s4fy98sof49gyh47e3w9yhf9os';
+$wgEnableUploads = true;
+
+wfLoadSkin( 'Vector' );
+$wgDefaultSkin = 'vector';
+
+wfLoadExtension( 'ParserFunctions' );
+
+// Needed for testing LuaSandbox on Special:Version
+wfLoadExtension( 'Scribunto' );
+
+// Needed for testing that VisualEditor is installed
+wfLoadExtension( 'VisualEditor' );
+
 // Avoid beta welcome popup that might interfere with the save button
 $wgVisualEditorShowBetaWelcome = false;
