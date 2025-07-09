9ad7dd869282f9001fc96ce93975bddf925c7d72
Vedmaka <god.vedmaka@gmail.com>
2025-07-10 00:48:17 +0200

    Add initial Dockerfile templates and configurations for MediaWiki setup
    - Introduce `base`, `core`, `skins`, `extensions`, `composer`, and `final` Dockerfile templates.
    - Configure essential services, dependencies, and environment variables for MediaWiki.
    - Add scripts, patches, and configurations to support MediaWiki setup and functionality.
    - Include `values.yml` to manage extensions, skins, and composer packages.

---
+++ template.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# builds a Dockerfile out of Dockerfile.tmpl
+
+docker run -it --rm \
+    --user "$(id -u)":"$(id -g)" \
+    -v "${PWD}:/build" \
+    -w "/build" \
+    hairyhenderson/gomplate:alpine --config .gomplate.yml --template templates/
