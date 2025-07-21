c47fffbb3f897984603d1ff20d6e04b5e830fccf
Vedmaka <god.vedmaka@gmail.com>
2025-07-21 17:56:56 +0200

    Restrict mounts for compile.sh

--- compile.sh
+++ compile.sh
@@ -2,8 +2,16 @@
 
 # compiles a Dockerfile out of Dockerfile.tmpl
 
+touch "${PWD}/Dockerfile"
+touch "${PWD}/_sources/configs/composer.wikiteq.json"
+
 docker run --rm \
-    --user "$(id -u)":"$(id -g)" \
-    -v "${PWD}:/build" \
+    -v "${PWD}/Dockerfile:/build/Dockerfile" \
+    -v "${PWD}/_sources/configs/composer.wikiteq.json:/build/_sources/configs/composer.wikiteq.json" \
+    -v "${PWD}/_sources/configs/composer.wikiteq.json.tmpl:/build/_sources/configs/composer.wikiteq.json.tmpl:ro" \
+    -v "${PWD}/templates:/build/templates:ro" \
+    -v "${PWD}/Dockerfile.tmpl:/build/Dockerfile.tmpl:ro" \
+    -v "${PWD}/.gomplate.yml:/build/.gomplate.yml:ro" \
+    -v "${PWD}/values.yml:/build/values.yml:ro" \
     -w "/build" \
     hairyhenderson/gomplate:alpine --config .gomplate.yml --template templates/
