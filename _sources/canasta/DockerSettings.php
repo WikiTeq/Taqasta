67b9e14560cd4ff2bf5b6285b4bb9d758255e5a7
Vedmaka <god.vedmaka@gmail.com>
2026-08-20 14:10:11 +0200

    Add AccountInfo, CleanChanges, MediaSearch, RelatedArticles, ShortDescription, and TemplateStylesExtender extensions to DOCKER_EXTENSIONS in DockerSettings.php

--- _sources/canasta/DockerSettings.php
+++ _sources/canasta/DockerSettings.php
@@ -38,6 +38,7 @@ const DOCKER_SKINS = [
 
 const DOCKER_EXTENSIONS = [
 	'SemanticMediaWiki', // keep it at the top to be enabled first, because some Semantic extension don't work in other case.
+	'AccountInfo',
 	'AJAXPoll',
 	'AbuseFilter',
 	'AddMessages',
@@ -61,6 +62,7 @@ const DOCKER_EXTENSIONS = [
 	'Cite', # bundled
 	'CiteThisPage', # bundled
 	'Citoid',
+	'CleanChanges',
 	'CodeEditor', # bundled
 	'CodeMirror',
 	'Collection',
@@ -128,6 +130,7 @@ const DOCKER_EXTENSIONS = [
 	'MassPasswordReset',
 	'Math',
 	'MediaUploader',
+	'MediaSearch',
 	'Mermaid',
 	'MobileDetect',
 	'MobileFrontend',
@@ -155,6 +158,7 @@ const DOCKER_EXTENSIONS = [
 	'Popups',
 	'PubmedParser',
 	'RegularTooltips',
+	'RelatedArticles',
 	'RemoteWiki',
 	'ReplaceText', # bundled
 	'RevisionSlider',
@@ -186,11 +190,13 @@ const DOCKER_EXTENSIONS = [
 	'Survey',
 	'SyntaxHighlight_GeSHi', # bundled
 	'Share',
+	'ShortDescription',
 	'Tabber',
 	'TabberNeue',
 	'Tabs',
 	'TemplateData', # bundled
 	'TemplateStyles',
+	'TemplateStylesExtender',
 	'TemplateWizard',
 	'TextExtracts', # bundled
 	'Thanks', # bundled
